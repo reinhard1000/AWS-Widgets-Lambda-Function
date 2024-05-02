be170ea26c323a97a5e320d9878bb44d332cb5ac
reinhard <boatengreinhard@gmail.com>
2024-05-01 22:30:23 -0400

    version 2

---
+++ widget.py
@@ -0,0 +1,17 @@
+import boto3
+import os
+import json
+
+s3 = boto3.client('s3');
+bucketName = os.environ.get('BUCKET');
+
+def main(event, context):
+    try:
+        method = event['httpMethod']
+
+        if(method == "GET"):
+            if event['path'] == '/':
+                data =s3.list_objects_v2(Bucket=bucketName)
+                body = {
+                    "widgets": [obj['Key'] for obj in data['Contents']]
+                }
