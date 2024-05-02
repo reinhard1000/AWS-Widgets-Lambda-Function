b661344630cf3d047480b645fdbd379df81e8441
Reinhard <87647952+reinhard1000@users.noreply.github.com>
2024-05-02 17:06:13 -0400

    Update widget_service.py

--- widget_service.py
+++ widget_service.py
@@ -5,3 +5,31 @@ from aws_cdk import(
     aws_s3 as as3
 )
 
+class WidgetService(cdk.Stack):
+    def __init__(self, scope:cdk.Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        bucket = as3.Bucket(self, "WidgetStore")
+
+        handler = afunc.Function(
+            self, 
+            "WidgetHandler",
+            runtime= afunc.Runtime.NODEJS_10_X,
+            code= afunc.Code.asset("resources"),
+            handler= "widgets.main",
+            environment= {"BUCKET": bucket.bucket_name}
+        )
+
+        api = apigateway.RestApi(
+            self, 
+            "widgets_api",
+            rest_api_name="Widget Service",
+            description= "This service serves widgets."
+        )
+
+        getWidgetsIntegration = apigateway.LambdaIntegration(
+            handler,
+            request_templates= {"application/json":'{"status":"200"}'}
+        )
+
+        api.root.add_method("GET", getWidgetsIntegration)
